305e9d2a9bdb24b612f1d888aebb4615a720872b
ABOATDev <30906528+ABOATDev@users.noreply.github.com>
2023-02-17 17:15:34 +0100

    change research

--- src/recherche/recherche.controller.ts
+++ src/recherche/recherche.controller.ts
@@ -1,18 +1,17 @@
-import { Controller,Get,Query} from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { RechercheService } from './recherche.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { SkipThrottle } from '@nestjs/throttler';
 
 @ApiTags('recherche')
 @Controller('recherche')
+@SkipThrottle()
+export class RechercheController {
+  constructor(private readonly rechercheService: RechercheService) {}
 
-export class RechercheController{
-    constructor(private readonly rechercheService: RechercheService) {}
-
-    @ApiOperation({ summary: 'Chercher un quizz' })
-    @Get()
-    findAll(@Query('term') term: string) {
-      return this.rechercheService.recherche(term);
-    }
-
+  @ApiOperation({ summary: 'Chercher un quizz' })
+  @Get()
+  findAll(@Query('term') term: string) {
+    return this.rechercheService.recherche(term);
+  }
 }
-
--- src/recherche/recherche.service.ts
+++ src/recherche/recherche.service.ts
@@ -3,16 +3,29 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class RechercheService {
-    prisma = new PrismaClient();
-    private readonly logger = new Logger(RechercheService.name);
+  prisma = new PrismaClient();
+  private readonly logger = new Logger(RechercheService.name);
 
-  async recherche(query: string){
+  async recherche(query: string) {
     const results = await this.prisma.quiz.findMany({
       where: {
         OR: [
           { title: { contains: query, mode: 'insensitive' } },
           //{ content: { contains: query, mode: 'insensitive' } },
         ],
+      },
+      include: {
+        creator: true,
+        questions: {
+          include: {
+            options: true,
+          },
+        },
+        _count: {
+          select: {
+            questions: true,
+          },
+        },
       },
     });
     return results;
